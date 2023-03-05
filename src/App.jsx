d61b79b38ca3eb8324752c844d9d2d221496a87e
Stephen Moran <stephenmoran@live.ie>
2023-03-05 16:28:05 +0000

    eslint & prettier setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Components
-import Home from './pages/Home';
-import Shop from './pages/Shop';
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -23,14 +23,14 @@ const App = () => {
           <h1>Coffee Shops - REST API</h1>
         </div>
         <Routes>
-          <Route path='/:id' element={<Shop />} />
-          <Route path='/' element={<Home />} />
+          <Route path="/:id" element={<Shop />} />
+          <Route path="/" element={<Home />} />
         </Routes>
       </QueryClientProvider>
     </BrowserRouter>
   );
 };
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
